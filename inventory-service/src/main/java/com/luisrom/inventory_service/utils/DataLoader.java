e199d4288322b86bf43d2a9db3413c3cb2b20542
luisrom661 <luisrom@unicauca.edu.co>
2023-09-10 20:49:06 -0500

    Done: First commit (Product, Order and Inventory microservices)

---
+++ inventory-service/src/main/java/com/luisrom/inventory_service/utils/DataLoader.java
@@ -0,0 +1,33 @@
+package com.luisrom.inventory_service.utils;
+
+import com.luisrom.inventory_service.model.entities.Inventory;
+import com.luisrom.inventory_service.repositories.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class DataLoader implements CommandLineRunner {
+    private final InventoryRepository inventoryRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("Loading data...");
+        if (inventoryRepository.findAll().isEmpty()){
+            inventoryRepository.saveAll(
+                    List.of(
+                            Inventory.builder().sku("000001").quantity(10L).build(),
+                            Inventory.builder().sku("000002").quantity(20L).build(),
+                            Inventory.builder().sku("000003").quantity(30L).build(),
+                            Inventory.builder().sku("000004").quantity(40L).build()
+                    )
+            );
+        }
+        log.info("Data loaded");
+    }
+}
---
+++ orders-service/src/main/java/com/luisrom/orders_service/repositories/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.luisrom.orders_service.repositories;
+
+import com.luisrom.orders_service.model.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
